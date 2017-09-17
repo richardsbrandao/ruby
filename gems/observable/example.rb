db00d781da7fe8b67c034526b09e42d7d9aeb2e5
Richard Brandão <richardsbrandao@gmail.com>
2017-09-16 23:01:24 -0300

    Observable studies

---
+++ gems/observable/example.rb
@@ -0,0 +1,89 @@
+require 'observer'
+
+class Checkout
+  def initialize(stock_manager)
+    @stock_manager = stock_manager
+  end
+
+  def run
+    loop do
+      product = @stock_manager.sample
+      product.buy!
+      break if @stock_manager.empty?
+      sleep 0.5
+    end
+  end
+end
+
+class StockManager < SimpleDelegator
+  def initialize(products)
+    super products
+    @products = products
+  end
+
+  # def sample
+  #   @products.sample
+  # end
+
+  # def empty?
+  #   @products.empty?
+  # end
+
+  # def pop(product)
+  #   @products.pop(product)
+  # end
+
+  def select!(product_to_remove)
+    @products.select! { |product| product.name != product_to_remove.name }
+  end
+end
+
+class Product 
+  include Observable
+  attr_reader :name, :quantity
+
+  def initialize(name, quantity)
+    @name, @quantity = name, quantity
+  end
+
+  def add!(quantity)
+    @quantity = @quantity + quantity
+  end
+
+  def buy!(quantity = 1)
+    @quantity = @quantity - quantity
+    if zero?
+      changed
+      notify_observers(Time.now, self)
+    end
+  end
+
+  private
+
+  def zero?
+    @quantity == 0
+  end
+end
+
+class EmptyStockObserver
+  def initialize(product, stock_manager)
+    @product, @stock_manager = product, stock_manager
+    @product.add_observer(self)
+  end
+
+  def update(now, product)
+    puts "#{product.name} is over at #{now}"
+    @stock_manager.select!(product)
+  end
+end
+
+products = [
+  Product.new('Livro 1', 5),
+  Product.new('Computador', 10),
+  Product.new('Geladeia', 6)
+]
+
+stock_manager = StockManager.new(products)
+checkout = Checkout.new(stock_manager)
+products.each { |product| EmptyStockObserver.new(product, stock_manager) }
+checkout.run
